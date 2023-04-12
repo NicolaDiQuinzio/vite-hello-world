e12b37feec11ecbd7d73c9b1bd84fb1071ad93e1
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-04-12 16:44:00 +0200

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,18 @@
+
+import Apptitle from './components/apptitle.vue';
+
+<template>
+
+  <Apptitle/>  
+
+</template>
+<script>
+import Apptitle from './components/apptitle.vue';
+import apptitle from './components/apptitle.vue';
+export default{
+    name: "App",
+    components: apptitle,
+    components: { Apptitle }
+}</script>
+
+<style lang="css" scoped></style>
---
+++ vite-hello-world/src/components/apptitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ text }}</h1>
+</template>
+<script>
+export default{
+name : 'apptitle',
+data(){
+    return{
+        text:'Ti prego funziona'
+    }
+}
+}
+</script>
+<style lang="css" scoped>
+
+
+</style>
